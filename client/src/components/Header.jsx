b7571f6a935dd8072e7bc9c5330ac6bd111a5a16
lululoveyuanyuan <xu.lulu@northeastern.edu>
2024-04-01 21:46:46 -0400

    Complete sign up page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
-      <div className='flex justify-between items-center container mx-auto p-4'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
           <span className='text-slate-800'>Selling</span>
           <span className='text-slate-600'>Sunset</span>
@@ -25,7 +25,10 @@ export default function Header() {
           <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
         </Link>
         <Link to='/sign-in'>
-          <li className='hidden sm:inline text-slate-700 hover:underline'>Sign in</li>
+          <li className='text-slate-700 hover:underline'>Sign in</li>
+        </Link>
+        <Link to='/sign-up'>
+          <li className='text-slate-700 hover:underline'>Sign up</li>
         </Link>
         </ul>
       </div>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignUp() {
   return (
-    <div>
-      SignUp
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form className='flex flex-col items-center space-y-3'>
+        <input type='text' placeholder='Username' className='border p-2 rounded-md' />
+        <input type='email' placeholder='Email' className='border p-2 rounded-md' />
+        <input type='password' placeholder='Password' className='border p-2 rounded-md' />
+        <button type='submit' className='bg-blue-500 text-white p-2 rounded-md  hover:opacity-80'>Sign Up</button>
+      </form>
+      <div className="flex gap-5 mt-7">
+        <p>Have an account?</p>
+        <Link to={'/sign-in'}>
+          <span className='text-blue-500'>Sign in</span>
+          </Link>
+      </div>
     </div>
   )
 }
