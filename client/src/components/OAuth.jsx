da81f4b9324b00457fed0f9ee8fc60e90ffe7c23
lululoveyuanyuan <xu.lulu@northeastern.edu>
2024-04-06 20:33:22 -0400

    amend-commit

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -3,7 +3,7 @@ import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import { app } from '../firebase';
 import { useDispatch } from 'react-redux';
 import { signInSuccess } from '../redux/user/userSlice';
-import { navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function OAuth() {
     const dispatch = useDispatch();
