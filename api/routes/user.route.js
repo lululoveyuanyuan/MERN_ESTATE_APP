fffc1dfc80a5edf695d9ad9c873de1032ec5ab58
lululoveyuanyuan <xu.lulu@northeastern.edu>
2024-04-09 16:27:20 -0400

    Update user API route created

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,6 +4,7 @@ import { test } from '../controllers/user.controller.js';
 const router = express.Router();
 
 router.get('/test', test);
+router.post('/update/:id', updateUder);
 
 
 export default router;
