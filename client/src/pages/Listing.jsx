8a6ea8f02a9bf060d00fddf1395b6f2add627ca1
lululoveyuanyuan <xu.lulu@northeastern.edu>
2024-04-15 11:20:49 -0400

    add Contact Landlord functionality to Listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -54,7 +54,7 @@ export default function Listing() {
     <main>
       {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
       {error && (
-        <p className='text-center my-7 text-2xl'>Something went wrong!</p>
+        <p className='text-center my-7 text-2xl'>Something went wrong..</p>
       )}
       {listing && !loading && !error && (
         <div>
